1ed2ddf2ecb9fd5e057b99c557f35a6091a02152
David Adegboruwa <G00420905@atu.ie>
2025-11-26 17:30:52 +0000

    Created Customerclass

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -7,7 +7,9 @@ public class Person {
 
     public Person()
     {
-
+        name = " ";
+        address = " ";
+        phoneNumber = " ";
     }
     public Person(String name, String address, String phoneNumber)
     {
